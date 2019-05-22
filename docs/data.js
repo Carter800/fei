e8b83b9be4e28ee9266d45d65c1d56573ead1ae3
ZeBei <zebeief@qq.com>
2019-05-22 10:31:16 +0800

    changed index.html

---
+++ docs/data.js
@@ -0,0 +1,4 @@
+var ks=[
+  {name:"扔瓶子高手",name_en:"Flip Bottle Master",img:"/images/flip-bottle.jpg",iosid:1222392314},
+  {name:"汉堡大师",name_en:"Burger Truck",img:"/images/burger.jpg",iosid:1450766819}
+];
